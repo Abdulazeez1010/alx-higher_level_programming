dcc89c0190f146e4d15da85734c68cc5f7464fca
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-08-10 23:07:52 -0700

    Added the 6-print_comb3.py file

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(48, 58):
+    j = i + 1
+    while j < 58:
+        if i == 56 and j == 57:
+            print("{}{}".format(chr(i), chr(j)))
+            break
+        print("{}".format(chr(i)), end="")
+        print("{}, ".format(chr(j)), end=" ")
+        j = j + 1
