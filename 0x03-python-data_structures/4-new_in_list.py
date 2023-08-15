3cdb2bd1ff9b584990bae6eaca3b824438b66357
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-08-14 21:01:27 -0700

    Modified the 4-new_in_list.py file

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
     new_list = []
-    for x in range(len(my_list)i):
+    for x in range(len(my_list)):
         new_list.append(my_list[x])
     new_list[idx] = element
     return new_list
