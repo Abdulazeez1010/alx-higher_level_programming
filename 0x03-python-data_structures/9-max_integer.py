2673d2a3acb6074974900cfddc8292a4c981a6df
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-08-14 21:25:15 -0700

    Added the 9-max_integer.py file

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    if my_list is None:
+        return None
+    max_int = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > max_int:
+            max_int = my_list[i]
+    return max_int
