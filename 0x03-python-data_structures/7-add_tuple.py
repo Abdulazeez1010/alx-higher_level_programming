55889eae51d27eae20bb497a7f8f869730efea3c
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-08-15 21:55:33 -0700

    Modified the 7-add_tuple.py file

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    return tuple(a + b for a,b in zip(tuple_a, tuple_b))
+    length_a = len(tuple_a)
+    length_b = len(tuple_b)
+    if length_a < 2:
+        for i in range(2 - length_a):
+            tuple_a = tuple_a + (0,)
+    if length_b < 2:
+        for i in range(2 - length_b):
+            tuple_b = tuple_b + (0,)
+    if length_a > 2:
+        tuple_a = tuple_a[:2]
+    if length_b > 2:
+        tuple_b = tuple_b[:2]
+    return tuple(a + b for a, b in zip(tuple_a, tuple_b))
