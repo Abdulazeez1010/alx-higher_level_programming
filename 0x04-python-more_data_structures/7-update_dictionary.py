57486dccb7cdb51a5edf49d4752dd7d274ff3fab
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-08-18 21:09:40 -0700

    Modified the 7-update_dictionary.py file

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    new_dictionary = a_dictionary
+    new_dictionary[key] = value
+    return new_dictionary
