ca657823de1794ade0321380e2c6f444cd01d0a2
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-08-21 09:54:19 -0700

    Added comment to follow Betty style

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 
 /**
+ * check_cycle - The function  checks if a singly linked list has a cycle in it
+ * @list : The list pointer
+ * Return: Returns 0 if there is no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
 	listint_t *ptr;
+
 	if (list == NULL)
 		return (0);
-	/*if (list->next == NULL)
-		return (0);*/
-	/*if (list->next == list)
-		return (1);*/
 
 	ptr = list;
 	while (ptr->next != NULL)
