efd701077ebe4f87d4a2155ea0a5c09f7333090f
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-08-21 07:36:04 -0700

    Modified the 10-check_cycle.c file

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,8 +7,8 @@ int check_cycle(listint_t *list)
 	listint_t *ptr;
 	if (list == NULL)
 		return (0);
-	if (list->next == NULL)
-		return (0);
+	/*if (list->next == NULL)
+		return (0);*/
 	/*if (list->next == list)
 		return (1);*/
 
