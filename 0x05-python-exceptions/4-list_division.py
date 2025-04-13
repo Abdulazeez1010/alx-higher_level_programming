bd17da67865d79ce5aa364c2dff64ce627d2a5ce
Abdulazeez1010 <alani.abdulazeez.akinpelumi@gmail.com>
2025-04-13 11:31:05 +0000

    Included a finally clause in the list division python file

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,17 +4,15 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         try:
             res = my_list_1[i]/my_list_2[i]
-            result.append(res)
         except (ZeroDivisionError):
             res = 0
-            result.append(res)
             print("division by 0")
         except (TypeError):
             res = 0
-            result.append(res)
             print("wrong type")
         except IndexError:
             res = 0
-            result.append(res)
             print("out of range")
+        finally:
+            result.append(res)
     return result
