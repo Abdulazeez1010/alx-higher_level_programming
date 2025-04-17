ed697c4974d8b9296a0bd4f54e9a6aad8f6df45c
Abdulazeez1010 <alani.abdulazeez.akinpelumi@gmail.com>
2025-04-17 08:51:12 +0000

    Modeified the 2-square python file

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-"""This module defines a Square class"""
+"""This module defines a Square class."""
 
 
 class Square:
-    """This class defines a square"""
+    """This class defines a square."""
 
     def __init__(self, size=0):
+        """Initialize the square with a given size.
+
+            Args:
+                size(int): The size of the square (must be >= 0).
+
+            Raises:
+                TypeError: If size is not an integer.
+                ValueError: If size < 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
