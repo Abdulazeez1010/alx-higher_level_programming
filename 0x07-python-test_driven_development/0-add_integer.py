ece16d31f7bb9b8ceac80f52f62eaef09364d4b2
Abdulazeez1010 <alani.abdulazeez.akinpelumi@gmail.com>
2025-04-23 17:08:01 +0000

    Modified the add_integer python and the add_integer text files

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,8 +3,24 @@
 
 
 def add_integer(a, b=98):
+    """Returns the sum of two integer.
+
+    Args:
+        a(int or float): The first number.
+        b(int or float): The second number.
+
+    Raises:
+        TypeError: If a or b  are not int or float.
+        OverflowError: If integer value is too large.
+
+    Returns:
+        int: The sum of a and b as an integer.
+    """
     if isinstance(a, bool) or not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if isinstance(b, bool) or not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return int(a)+int(b)
+    try:
+        return int(a)+int(b)
+    except(OverflowError):
+        raise OverflowError("integer value too large")
