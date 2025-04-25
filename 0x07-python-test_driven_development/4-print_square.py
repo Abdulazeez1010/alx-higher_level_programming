abff511d9237cca3bfd5cd27f2ec3f62addb022c
Abdulazeez1010 <alani.abdulazeez.akinpelumi@gmail.com>
2025-04-25 06:39:11 +0000

    Modified 4-print_square.py to fit pycode style

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""This module defines """
+"""This module defines a function that prints a square"""
 
 
 def print_square(size):
-    """Prints a square
+    """Prints a square.
 
+    Args:
+        size (int): size of the square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        TypeError: If size is less than 0.
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
