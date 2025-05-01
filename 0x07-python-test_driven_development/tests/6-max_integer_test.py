99bc2d356c298d79c14cd1a037276bc0fbdea680
Abdulazeez1010 <alani.abdulazeez.akinpelumi@gmail.com>
2025-05-01 06:07:20 +0000

    Modified the 6-max_integer_test.py unittest test file

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,10 +8,14 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
         list_0 = []
-        list_1 = [1, 8, 2, 7, 3, 6]
-        list_2 = [4, 1, -2, 1, 0]
-        list_3 = [-5, -3, -2, -7]
-        self.assertEqual(8, max_integer(list_1))
-        self.assertEqual(4, max_integer(list_2))
+        list_1 = [6]
+        list_2 = [1, 8, 2, 7, 3, 6]
+        list_3 = [4, 1, -2, 1, 0]
+        list_4 = [-5, -3, -2, -7]
+        list_5 = [1, 3, 5, 2, 9]
         self.assertEqual(None, max_integer(list_0))
-        self.assertEqual(-2, max_integer(list_3))
+        self.assertEqual(6, max_integer(list_1))
+        self.assertEqual(8, max_integer(list_2))
+        self.assertEqual(4, max_integer(list_3))
+        self.assertEqual(-2, max_integer(list_4))
+        self.assertEqual(9, max_integer(list_5))
