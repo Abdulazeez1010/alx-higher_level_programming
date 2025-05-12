9fe0cdedce30e865f3b967f668d071f5e35e4092
Abdulazeez1010 <alani.abdulazeez.akinpelumi@gmail.com>
2025-05-12 07:38:03 -0700

    Modified the 1-my_list.py file

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,7 +3,8 @@
 
 
 class MyList(list):
-    """A custom list class that adds a method to print the list in sorted order.
+    """A custom list class that adds a method to print the list in sorted
+    order.
 
     Inherits from:
         list: The built-in list class.
