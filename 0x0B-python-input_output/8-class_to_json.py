4a5a7c59bc087c4583e5a8dcfd380681ede7d890
Abdulazeez1010 <alani.abdulazeez.akinpelumi@gmail.com>
2025-05-18 13:33:42 -0700

    Added the 8-class_to_json.py file

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module defines a function that returns the dictionary description
+    with simple data structure for JSON serialization of an object.
+"""
+
+import json
+
+
+def class_to_json(obj):
+    """Returns the dictionary description with simple data structure (list,
+        dictionary, string, integer and boolean) for JSON serialization of
+        an object.
+
+    Args:
+        obj: An instance of a class.
+    """
+    return obj.__dict__
