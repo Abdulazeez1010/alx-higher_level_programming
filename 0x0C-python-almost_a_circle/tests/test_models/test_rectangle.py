23b64ac46b01a16822e37c6c7796516561500ab3
Abdulazeez1010 <alani.abdulazeez.akinpelumi@gmail.com>
2025-05-23 09:07:32 -0700

    Added the test_rectangle.py file

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""The unittest module for the Rectangle class"""
+
+
+import unittest
+
+
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """Unit rest for the Rectangle class"""
+    def test_rectangle(self):
+        """Test cases to validate correct output for Rectangle"""
+        r1 = Rectangle(10, 2)
+        r2 = Rectangle(2, 10)
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        r4 = Rectangle(5, 8)
+
+        self.assertEqual(1, r1.id)
+        self.assertEqual(2, r2.id)
+        self.assertEqual(12, r3.id)
+        self.assertEqual(3, r4.id)
