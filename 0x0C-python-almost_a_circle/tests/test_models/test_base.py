bf30b93745971d4a5c5e7ef5835b849316b68c53
Abdulazeez1010 <alani.abdulazeez.akinpelumi@gmail.com>
2025-05-25 04:43:43 -0700

    Added test for save_to_file method

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,7 +5,6 @@
 import unittest
 import json
 
-
 from models.base import Base
 from models.rectangle import Rectangle
 
@@ -37,3 +36,13 @@ class TestBase(unittest.TestCase):
         result = Base.to_json_string([dictionary])
         expected_output = '[{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}]'
         self.assertEqual(sorted(expected_output), sorted(result))
+
+    def test_json_string_to_file(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+        with open("Rectangle.json", "r") as file:
+            content = file.read()
+        data = json.loads(content)
+        expected_output = [{"y": 8, "x": 2, "id": 1, "width": 10, "height": 7}, {"y": 0, "x": 0, "id": 2, "width": 2, "height": 4}]
+        self.assertEqual(expected_output, data)
